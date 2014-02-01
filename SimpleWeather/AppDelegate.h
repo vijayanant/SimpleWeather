39a2c7afbe4113948bea0afca3ef358c9223cb68
Vijay Ananth <vijay@simpragma.com>
2014-02-01 17:15:22 +0530

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Vijay Ananth on 01/02/2014.
+//  Copyright (c) 2014 Vijay Ananth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Vijay Ananth on 01/02/2014.
+//  Copyright (c) 2014 Vijay Ananth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
